0300e44b9723e2d56875919b1e812e9eff6b9aa3
alexdelorenzo <alex@alexdelorenzo.dev>
2021-12-28 23:18:18 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ readme = Path('README.md').read_text()
 
 
 setup(name="limiter",
-      version="0.2.0.post1",
+      version="0.3.0",
       description="⏲️ Easy rate limiting for Python. Rate limiting async and thread-safe decorators and context-managers using the token bucket algorithm.",
       long_description=readme,
       long_description_content_type="text/markdown",
