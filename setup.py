4103f001bf95f3a6ee4fbf286914f6c0376a9677
alexdelorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2024-02-13 23:52:14 -0500

    Format consistently

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 from setuptools import setup
 from pathlib import Path
 
+
 requirements = Path('requirements.txt').read_text().split('\n')
 readme = Path('README.md').read_text()
 
-
-setup(name="limiter",
-      version="0.3.1",
-      description="⏲️ Easy rate limiting for Python. Rate limiting async and thread-safe decorators and context managers that use a token bucket algorithm.",
-      long_description=readme,
-      long_description_content_type="text/markdown",
-      url="https://github.com/alexdelorenzo/limiter",
-      author="Alex DeLorenzo",
-      license="AGPL-3.0",
-      packages=['limiter'],
-      zip_safe=True,
-      install_requires=requirements,
-      keywords="rate-limit rate limit token bucket token-bucket token_bucket tokenbucket decorator contextmanager asynchronous threadsafe synchronous".split(' '),
-      python_requires='>=3.10',
+setup(
+  name="limiter",
+  version="0.4.0",
+  description="⏲️ Easy rate limiting for Python. Rate limiting async and thread-safe decorators and context managers "
+              "that use a token bucket algorithm.",
+  long_description=readme,
+  long_description_content_type="text/markdown",
+  url="https://github.com/alexdelorenzo/limiter",
+  author="Alex DeLorenzo",
+  license="AGPL-3.0",
+  packages=['limiter'],
+  zip_safe=True,
+  install_requires=requirements,
+  keywords="rate-limit rate limit token bucket token-bucket token_bucket tokenbucket decorator contextmanager "
+           "asynchronous threadsafe synchronous".split(' '),
+  python_requires='>=3.10',
 )
