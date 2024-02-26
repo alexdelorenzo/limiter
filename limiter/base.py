bb46cd45341907b63779b1d33188f96c92b57769
alexdelorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2024-02-26 17:10:58 -0500

    Support `+/-` jitter directions, add debug statements

--- limiter/base.py
+++ limiter/base.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
-from random import randrange
+import logging
+from operator import add, sub
+from random import choice, randrange
 from typing import Awaitable, Callable, Final, ParamSpec, TypeVar
 
 from token_bucket import Limiter as TokenBucket, MemoryStorage
+
+
+log = logging.getLogger(__name__)
 
 
 WAKE_UP: Final[int] = 0
@@ -24,6 +29,7 @@ Duration = Num
 UnitsInSecond = Duration
 JitterRange = range | tuple[int, int] | tuple[int, int, int]
 Jitter = int | bool | JitterRange
+JitterDirection = Callable[[Num, Num], Num]
 
 
 CONSUME_TOKENS: Final[Tokens] = 1
@@ -34,6 +40,8 @@ MS_IN_SEC: Final[UnitsInSecond] = 1000
 DEFAULT_BUCKET: Final[Bucket] = b"default"
 DEFAULT_JITTER: Final[Jitter] = False
 DEFAULT_RANGE: Final[range] = range(50)
+
+JITTER_DIRECTIONS: Final[tuple[JitterDirection, ...]] = add, sub
 
 
 def _get_bucket(name: BucketName) -> Bucket:
@@ -55,7 +63,7 @@ def _get_limiter(rate: Tokens = RATE, capacity: Tokens = CAPACITY) -> TokenBucke
   return TokenBucket(rate, capacity, MemoryStorage())
 
 
-def _get_bucket_limiter(bucket: BucketName, limiter: 'Limiter') -> tuple[Bucket, TokenBucket]:
+def _get_bucket_limiter(bucket: BucketName, limiter: 'Limiter' | TokenBucket) -> tuple[Bucket, TokenBucket]:
   bucket: Bucket = _get_bucket(bucket)
 
   if not isinstance(limiter, TokenBucket):
@@ -72,25 +80,29 @@ def _get_sleep_duration(
   units: UnitsInSecond = MS_IN_SEC
 ) -> Duration:
   """Increase contention by adding jitter to sleep duration"""
+  log.debug(f'{consume=}, {tokens=}, {rate=}, {jitter=}, {units=}')
   duration: Duration = (consume - tokens) / rate
+  log.debug(f'{duration=}')
+
+  operation = choice(JITTER_DIRECTIONS)
 
   match jitter:
     case int() | float():
-      return duration - jitter
+      return operation(duration, jitter)
 
     case range() | bool() if jitter:
       if jitter is True:
         jitter: range = DEFAULT_RANGE
 
       amount: Duration = randrange(jitter.start, jitter.stop, jitter.step) / units
-      return duration - amount
+      return operation(duration, amount)
 
     case start, stop:
       amount: Duration = randrange(start, stop) / units
-      return duration - amount
+      return operation(duration, amount)
 
     case start, stop, step:
       amount: Duration = randrange(start, stop, step) / units
-      return duration - amount
+      return operation(duration, amount)
 
   return duration
